c4d100ca88af31048b6b63f8b015dc0fcee43c53
Jason Thronson <jason.thronson99@gmail.com>
2024-11-19 20:19:54 -0600

    idk

--- endeavour-lxqt.sh
+++ endeavour-lxqt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Non-Aur Packages
-sudo pacman -S --needed lxqt podman-docker distrobox xfwm4 exa qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator flameshot nemo obsidian thunderbird calibre syncthing evince quodlibet nicotine+ yt-dlp bat helix atuin fzf unarchiver btop ncdu tealdeer brightnessctl cmake clang lazygit zellij pinta
+sudo pacman -S --needed lxqt xfwm4 podman-docker distrobox qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator flameshot nemo obsidian thunderbird calibre syncthing evince quodlibet nicotine+ yt-dlp bat helix atuin fzf unarchiver btop ncdu tealdeer brightnessctl cmake clang lazygit zellij pinta
 
 # Install Aur Packages
 yay -S --needed quickemu arcolinux-logout megasync-bin visual-studio-code-bin icu69 stremio floorp-bin appimagelauncher proton-vpn-gtk-app

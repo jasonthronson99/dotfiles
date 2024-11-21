61eea45e721c991114c84321a05bddf4b77a3389
Jason Thronson <jason.thronson99@gmail.com>
2024-11-20 22:49:24 -0600

    test commit

--- endeavour-lxqt.sh
+++ endeavour-lxqt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Non-Aur Packages
-sudo pacman -S --needed lxqt xfwm4 podman-docker distrobox qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator flameshot nemo obsidian thunderbird calibre syncthing evince quodlibet nicotine+ yt-dlp bat helix atuin starship fzf unarchiver btop ncdu tealdeer brightnessctl cmake clang lazygit zellij pinta
+sudo pacman -S --needed lxqt xfwm4 podman-docker distrobox qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator flameshot nemo obsidian thunderbird calibre syncthing evince quodlibet nicotine+ yt-dlp bat helix atuin starship fzf unarchiver btop ncdu tealdeer brightnessctl cmake clang lazygit tmux nnn pinta
 
 # Install Aur Packages
-yay -S --needed quickemu arcolinux-logout megasync-bin visual-studio-code-bin icu69 stremio floorp-bin appimagelauncher proton-vpn-gtk-app
+yay -S --needed quickemu arcolinux-logout megasync-bin visual-studio-code-bin icu69 stremio floorp-bin proton-vpn-gtk-app
 
 # Hyprland Setup (Non-Aur Packages)
 sudo pacman -S --needed hyprland waybar swaybg

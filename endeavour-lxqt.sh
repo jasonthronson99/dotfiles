dfa9491ce0221fef706e346e3ef90d870d09bb07
Jason Thronson <jason.thronson99@gmail.com>
2024-11-26 03:55:49 -0600

    added ls

--- endeavour-lxqt.sh
+++ endeavour-lxqt.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# Install Terminal Utilities
+sudo pacman -S --needed bat fzf unarchiver tealdeer brightnessctl atuin
+# Install Developer Programs
+sudo pacman -S --needed podman-docker distrobox helix
+yay -S --needed visual-studio-code-bin icu69 
+# Productivity Packages
+sudo pacman -S --needed thunderbird obsidian flameshot nemo okular
+yay -S --needed localsend  
+
+# Entertainment Packages
+sudo pacman -S --needed quodlibet nicotine+
+yay -S --needed stremio 
 # Install Non-Aur Packages
-sudo pacman -S --needed lxqt xfwm4 podman-docker distrobox qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator flameshot nemo obsidian thunderbird calibre syncthing zathura zathura-pdf-mupdf evince quodlibet nicotine+ yt-dlp bat helix atuin starship fzf unarchiver btop ncdu tealdeer brightnessctl cmake clang lazygit tmux nnn pinta
+sudo pacman -S --needed lxqt xfwm4 qbittorrent rofi vivaldi vivaldi-ffmpeg-codecs alacritty kitty galculator calibre yt-dlp helix starship ncdu cmake clang lazygit tmux nnn pinta
 
 # Install Aur Packages
-yay -S --needed quickemu arcolinux-logout megasync-bin visual-studio-code-bin icu69 stremio floorp-bin proton-vpn-gtk-app
-
-# Hyprland Setup (Non-Aur Packages)
+yay -S --needed quickemu arcolinux-logout megasync-bin floorp-bin proton-vpn-gtk-app
+# LXQT Desktop
+sudo pacman -S --needed lxqt xfwm4
+# Basic Window Manager Utilities
+sudo pacman -S --needed rofi
+yay -S --needed arcolinux-logout
+# Hyprland Packages
 sudo pacman -S --needed hyprland waybar swaybg
-# Hyprland Setup (Aur Packages)
 yay -S --needed hyprshade

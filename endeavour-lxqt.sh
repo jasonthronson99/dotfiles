324aa31a737eb3029ae9e1fa5575a52a5bf906b9
Jason Thronson <jason.thronson99@gmail.com>
2024-12-02 11:33:43 -0600

    got rid of ls.py

--- endeavour-lxqt.sh
+++ endeavour-lxqt.sh
@@ -11,14 +11,17 @@ yay -S --needed visual-studio-code-bin icu69
 sudo pacman -S --needed vivaldi vivaldi-ffmpeg-codecs thunderbird obsidian flameshot nemo okular galculator qbittorrent pinta
 yay -S --needed localsend megasync-bin proton-vpn-gtk-app zen-browser-avx2-bin
 
-# GUI ENTERTAINMENT APPS
-sudo pacman -S --needed quodlibet nicotine+
-yay -S --needed stremio 
+# GUI ENTERTAINMENT (NON-GAMING) APPS
+sudo pacman -S --needed vlc mpv quodlibet nicotine+ 
+yay -S --needed stremio
 
 # DESKTOP & WINDOW MANAGER UTILITIES
-sudo pacman -S --needed lxqt xfwm4 rofi polybar waybar swaybg 
+sudo pacman -S --needed feh rofi polybar waybar swaybg 
 yay -S --needed arcolinux-logout 
 
 # HYPRLAND
 sudo pacman -S --needed hyprland 
 yay -S --needed hyprshade
+
+# i3
+sudo pacman -S --needed i3-wm

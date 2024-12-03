f6fb7bfc76f6c194c1e3bf88bcddb680e78b01df
Jason Thronson <jason.thronson99@gmail.com>
2024-12-02 20:53:27 -0600

    Got Rid Of Hypr

--- arch-packages.sh
+++ arch-packages.sh
@@ -9,7 +9,7 @@ yay -S --needed visual-studio-code-bin icu69
 
 # GUI UTILITIES & PRODUCTIVITY APPS
 sudo pacman -S --needed vivaldi vivaldi-ffmpeg-codecs thunderbird obsidian flameshot nemo okular galculator qbittorrent pinta
-yay -S --needed localsend megasync-bin proton-vpn-gtk-app zen-browser-avx2-bin
+yay -S --needed localsend megasync-bin proton-vpn-gtk-app zen-browser-avx2-bin thorium-browser-bin
 
 # GUI ENTERTAINMENT (NON-GAMING) APPS
 sudo pacman -S --needed vlc mpv quodlibet nicotine+ 
@@ -19,9 +19,3 @@ yay -S --needed stremio
 sudo pacman -S --needed feh rofi polybar waybar swaybg 
 yay -S --needed arcolinux-logout 
 
-# HYPRLAND
-sudo pacman -S --needed hyprland 
-yay -S --needed hyprshade
-
-# i3
-sudo pacman -S --needed i3-wm

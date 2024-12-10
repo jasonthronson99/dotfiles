4edd8801566f9c77f6219f453acdc9ba5d4d43df
Jason Thronson <jason.thronson99@gmail.com>
2024-12-10 15:56:53 -0600

    Zwm Is Unstable

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 # TERMINAL UTILITIES
-sudo pacman -S --needed alacritty kitty bat fzf unarchiver tealdeer brightnessctl atuin yt-dlp tmux gdu micro btop
+sudo pacman -S --needed alacritty kitty bat fzf unarchiver tealdeer brightnessctl atuin yt-dlp tmux gdu micro helix btop sc 
 
 # DEVELOPER PROGRAMS
-sudo pacman -S --needed podman-docker distrobox helix ruff-lsp python-black pyright cmake clang lazygit
+sudo pacman -S --needed podman-docker distrobox ruff-lsp python-black pyright cmake clang lazygit vscode-html-languageserver vscode-css-languageserver lua-language-server typescript-language-server
 yay -S --needed visual-studio-code-bin icu69 
 
 # GUI UTILITIES & PRODUCTIVITY APPS
-sudo pacman -S --needed vivaldi vivaldi-ffmpeg-codecs obsidian flameshot nemo okular galculator qbittorrent pinta bitwarden zathura zathura-pdf-mupdf 
+sudo pacman -S --needed obsidian flameshot nemo okular galculator qbittorrent pinta zathura zathura-pdf-mupdf 
 yay -S --needed localsend betterbird-bin megasync-bin proton-vpn-gtk-app zen-browser-avx2-bin
 
-# GUI ENTERTAINMENT (NON-GAMING) APPS
+# GUI ENTERTAINMENT APPS
 sudo pacman -S --needed vlc mpv quodlibet nicotine+ 
 yay -S --needed stremio
 
 # DESKTOP & WINDOW MANAGER UTILITIES
-sudo pacman -S --needed feh rofi polybar waybar swaybg parcellite lxqt-powermanagement volumeicon
+sudo pacman -S --needed network-manager-applet feh rofi polybar waybar swaybg parcellite lxqt-powermanagement volumeicon gnome-tweaks lxappearance sddm
 yay -S --needed arcolinux-logout 
 
-# EXTRA/BACKUP WINDOW MANAGER
-yay -S --needed zwm-git
-mv zwm.desktop /usr/share/xsessions/
+sudo systemctl enable sddm.service
